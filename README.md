a18b4bc53492781aaf0571aaf4bc6716f0a9ca04
pablogormi <pablogormi822@gmail.com>
2018-03-16 14:36:39 -0300

    Finish program
    and update instructions

--- README.md
+++ README.md
@@ -11,5 +11,19 @@ In my case this is "C:\ffmpeg\bin", but it may change.
 
 If you are using Linux, you probably know enough to be able to do it.
 
-Requires [MakeBAX](https://gbatemp.net/threads/release-bax-boot-animations-for-your-3ds.497717/), **in the same folder as the file you want to convert and in the same folder as MakeBAX-Multi**. I don't know if I'm allowed to share MakeBAX here, so just download it from there. It's a .exe that you just need to put on your folder.
+Requires [MakeBAX](https://gbatemp.net/threads/release-bax-boot-animations-for-your-3ds.497717/), **in the same folder as the file you want to convert and in the same folder as MakeBAX-Multi OR in your PATH**, renamed as "makebax" (**not** case sensitive). I don't know if I'm allowed to share MakeBAX here, so just download it from there. It's a .exe that you just need to put on your folder.
 
+# Usage
+
+1. Make sure MakeBAX-Multi is in the same folder as MakeBAX.exe and the file you want to convert.
+2. Open up cmd (not required but recommended) and cd to that folder
+3. Double click on the program (or load it from cmd if you did step 2, just write the name of the program and press enter)
+4. Follow on-screen instructions
+5. Wait until the program has finished.
+6. The .bax should be there. If there's anything not working tell me.
+7. Profit!
+
+# Building it
+
+Browse to the /source/ folder and do "g++ main.cpp -o MakeBAX-Multi.exe"
+Easy.
--- source/main.cpp
+++ source/main.cpp
@@ -13,8 +13,9 @@ bool fileExist(const string& fileName)
 
 
 int main() {
+	system("echo off");
 	char version[] = "1.0";
-	string filename, outname, screen, framerate;
+	string filename, outname, screen, framerate, outfile;
 	
 	// ask for stuff
 	cout << "BAX Converter v" << version << endl;
@@ -30,6 +31,32 @@ int main() {
 	cout << "Enter output file name (no extension)" << endl;
 	cout << "> ";
 	cin >> outname;
+	
+	//Check for .ivf file
+	std::string::size_type idx;
+	idx = filename.rfind('.');
+	std::string extension = filename.substr(idx+1);
+	cout << "Your file extension is " << extension << endl;
+	
+	if (extension == "ivf") {
+		outfile = filename;
+		string baxcomm = "makebax " + outfile + " " + outname + ".bax";
+		string res;
+		cout << "IVF file found! Is the resolution correct? " << endl;
+		cout << "y/n > ";
+		cin >> res;
+		if (res == "y") {
+			cout << "Ok, skipping ffmpeg and using MakeBAX..." << endl;
+			cout << "Command to be used: " << baxcomm << endl;
+			sleep(1);
+			system(baxcomm.c_str());
+			return 0;
+		} else if (res == "no") {
+			cout << "Ok, putting it through ffmpeg... ";
+			sleep(1);
+		}
+	}
+	
 	cout << "Select screen for the video" << endl;
 	cout << "1. Top Screen" << endl;
 	cout << "2. Bottom Screen" << endl;
@@ -48,29 +75,50 @@ int main() {
 		return 1;
 	}
 	
-	// Check stuff
-	cout << "You want to convert " << filename << " to " << outname << endl;
-
-	std::string::size_type idx;
-
-	idx = filename.rfind('.');
-
-	std::string extension = filename.substr(idx+1);
+	// Check for .gif and kick you out of the program because I'm too lazy
+	if (extension == "gif") {
+		cout << "Gif found! Can't convert directly to ivf. Converting to mp4... " << endl;
+		sleep(1);
+		string gifcmd = "ffmpeg -i " + filename + " -b:v 1M -r " + framerate + " " + outname + ".mp4";
+		system(gifcmd.c_str());
+		system("cls");
+		cout << " " <<endl;
+		cout << " " <<endl;
+		cout << " " <<endl;
+		cout << "I've converted " << filename << " to a MP4 file." << endl;
+		cout << "Please run this program again but using " << outname << ".mp4 as input. Thank you!";
+		
+		return 0;
+	}
 	
-	cout << "Your file extension is " << extension << endl;
+	// Check stuff
+	cout << "You want to convert " << filename << " to " << outname << ".bax" << " at " << framerate << " fps." << endl;
 
+	
+	// Remind me of adding comments
 
+	
+	outfile = outname + ".ivf";
+	string baxcomm = "makebax " + outfile + " " + outname + ".bax";
 	string conversion = "ffmpeg -i " + filename + " -f ivf -vf scale=" + screen + ":240,fps=" + framerate + " " + outname + ".ivf";
 	cout << "The command used will be: " << endl;
 	cout << conversion << endl;
+	cout << "Starting mp4 conversion with ffmpeg..." << endl;
 	
 	// Unnecessary wait time :D
 	sleep(1);
 	
-	cout << "Starting mp4 conversion with ffmpeg..." << endl;
-	
 	const char *command = conversion.c_str();
 	
 	system(command);
-
+	
+	cout << "Conversion to .ivf finished! starting conversion to .bax..." << endl;
+	cout << "Command to be used: " << baxcomm << endl;
+	sleep(1);
+	system(baxcomm.c_str());
+	remove(outfile.c_str());
+	
+	cout << "Done! Your output file is named " << outname << ".bax!" << endl;
+	system("pause");
+	return 0;
 }
